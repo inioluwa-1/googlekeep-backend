5366a9ec22a7d775039606099bd437cfdbf38f26
ayanbisi inioluwa <ifeini048@gmail.com>
2026-05-05 13:58:25 +0100

    url update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  '',
+  'https://googlekeep-frontend.vercel.app/',
   process.env.CLIENT_URL
 ].filter(Boolean)
 
